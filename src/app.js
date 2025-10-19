dae7903a6a9af4fee9ef9c7f8577fc278fbd8017
Waleed <lido.murad@gmail.com>
2025-10-20 00:23:48 +0400

    Writting autho middleware

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
 const express = require('express');
 
+const { adminAuth, userAuth } = require('./middlewares/auth');
+
 const app = express();
 
-app.use(
-  '/user',
-  (req, res, next) => {
-    // Route Handler ().
-    // res.send('Rounte Handler 1');
-    console.log('Handling the rout user');
-    next();
-    res.send('Respone!!!');
-  },
-  (req, res) => {
-    // Route Handler 2.
-    console.log('Handling the rout user 2!!');
-    res.send('2nd Respone!!!');
-  }
-);
+// Handle auth middleware for all requests (GET,PATCH, POST,PATCH).
+app.use('/admin', adminAuth);
+
+app.post('/user/login', (req, res) => {
+  res.send('User logged in successfully!');
+});
+
+app.get('/user', userAuth, (req, res) => {
+  res.send('User all data');
+});
+
+app.get('/admin/getAllData', (req, res) => {
+  res.send('Get all data');
+});
+
+app.get('/admin/deleteUser', (req, res) => {
+  res.send('Deleted a user');
+});
 
 app.listen(7777, () => {
   console.log('Server is successfully listening on port 7777...');
