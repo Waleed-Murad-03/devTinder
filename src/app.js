a76261288eba059b31258aab372f9dae6d6217f4
Waleed <lido.murad@gmail.com>
2025-10-19 12:04:10 +0400

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,24 +2,11 @@ const express = require('express');
 
 const app = express();
 
-// This will only handle get http call to /user
-app.get('/user', (req, res) => {
+// In here basically whenever we add a '?' it means it is optional.
+// So in here the /abc route and also /ac route will work.
+app.get('/user/:userId/:name/:password', (req, res) => {
+  console.log(req.params);
   res.send({ firstName: 'Lido', lastName: 'Murad' });
-});
-
-app.post('/user', (req, res) => {
-  // Saving data to db
-  res.send('Data successfully saved to the database');
-});
-
-app.delete('/user', (req, res) => {
-  res.send('Deleted Successfully');
-});
-
-// 'use' will match all of the HTTP method API calls to /test
-app.use('/test', (req, res) => {
-  // This function right here is known as a request handler
-  res.send('Hello from the server');
 });
 
 app.listen(7777, () => {
