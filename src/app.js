87572ae810e45c6f05d1db6dab060c7533bd69c1
Waleed <lido.murad@gmail.com>
2025-10-20 21:46:24 +0400

    Delete and Update user APIs

--- src/app.js
+++ src/app.js
@@ -49,6 +49,31 @@ app.get('/feed', async (req, res) => {
   }
 });
 
+// Deelete a user from the database
+app.delete('/user', async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+
+    res.send('User deleted successfully');
+  } catch (err) {
+    res.status(400).send('Something went wrong');
+  }
+});
+
+// Update data of the user
+
+app.patch('/user', async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send('User updated Successfully');
+  } catch (err) {
+    res.status(400).send('Something went wrong');
+  }
+});
+
 connectDB()
   .then(() => {
     console.log('database connected successfully');
