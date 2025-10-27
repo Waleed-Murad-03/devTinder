605d1a83a7f52b2c871117fb19b2ed9269607120
Waleed <lido.murad@gmail.com>
2025-10-27 11:24:41 +0400

    Added the POST /request/review/:status/:requestId API with proper validations

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,8 +3,11 @@ const requestRouter = express.Router();
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
 const User = require('../models/user');
+const ConnectionRequestModel = require('../models/connectionRequest');
 
 // This api is only for interested  or ignored api. So that status in here, it is only interested or ignored. Basically right swipe for accepted or left swipe for ignore.
+//  POST /request/send/:status/:requestId
+// This API is for the senders POV
 requestRouter.post(
   '/request/send/:status/:toUserId',
   userAuth,
@@ -68,4 +71,47 @@ requestRouter.post(
   }
 );
 
+// Now next we will do it for
+// This api is for the receivers POV
+//  POST /request/review/:status/:requestId
+requestRouter.post(
+  '/request/review/:status/:requestId',
+  userAuth,
+  async (req, res) => {
+    // Akshay is sending to Elon
+    // Is Elon LoggedInUser (elon is the receiver and he himself the toUserId person is authorized to accept it)
+    //  Status should be interested because it it was ingored it will be skipped. So there is only one status we will allow.
+    try {
+      const loggedInUser = req.user; // We are getting this from the userAuth
+      const { status, requestId } = req.params; // this is coming from the url from postman
+
+      // Validate the status
+      const allowedStatus = ['accepted', 'rejected'];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ message: 'Status not allowed!' });
+      }
+
+      // Validate the userId is in the database
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: 'interested', //only the interested statuses will be checked. We are ignoring any request in the db that has rejected
+      });
+
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: 'Connection request not found' });
+      }
+
+      // If we have passed all of the checks then we can safely change the status
+      connectionRequest.status = status;
+
+      const data = await connectionRequest.save();
+      res.json({ message: 'Connection request ' + status, data });
+    } catch (err) {
+      res.status(400).send('Error: ' + err.message);
+    }
+  }
+);
 module.exports = requestRouter;
