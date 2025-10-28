acd6213d126c6f6aee8777ba5040a41a604f9b6e
Waleed <lido.murad@gmail.com>
2025-10-28 11:38:54 +0400

    Created the GET /feed API and I would safely say I mastered querying  from this API

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
-
 const userRouter = express.Router();
+const User = require('../models/user');
 
 const USER_SAFE_DATA = 'firstName lastName photoUrl age gender about skills';
 
@@ -56,6 +56,48 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
     res.json({ data });
   } catch (err) {
     res.status(400).send({ message: err.message });
+  }
+});
+
+// So we need to see the people who we are not connected with yet.
+// We need to avoid the people who we have already ignored
+// We should not see users who we are already interested in
+// We should not see our own card as well\ // User should see all the cards except
+// 0. his own card
+// 1. his connections
+// 2. ignored people
+// 3. Already send the connection request to
+// We will only see the users cards on our fields which we did not interact with or like there isnt any sort of entry
+userRouter.get('/feed', userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    // Find all of the connection requests that either I have sent or received so that we do not see them on our feed
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select('fromUserId toUserId');
+    // .populate('fromUserId', 'firstName')
+    // .populate('toUserId', 'firstName'); //These are the people that I bascially do not want in my feed. They are all of the peope who either donald soendsomething to or donald have received from.
+
+    // SET is a data structure similar to arrays but if we try to add 2 times the same values it will not count the second.So it is  a set or list of unique values.
+    // No duplicates. So now we will get a list of uniqe people whose profile should not be shown
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+    // console.log(hideUsersFromFeed);
+    // Now that we know ho are the people that we should hide from the feed
+    // So basically in the query below I am basically finding all of the users whose id is not present in the hideUsersFromFeed set list. We are doing kind of the reverse here
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } }, //We are finnding all of the users who are not in the hideUsersFromFeed array and their id is not equal to the logged in user id
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA);
+
+    res.send(users);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
