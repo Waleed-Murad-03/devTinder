cd10e15ae7697c77479c62ba71ece5513531ccaf
Waleed <lido.murad@gmail.com>
2025-11-02 13:58:44 +0400

    Now we are also generting the token once uers sign up

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -20,8 +20,13 @@ authRouter.post('/signup', async (req, res) => {
       password: passwordHash,
     });
 
-    await user.save();
-    res.send('User added successfully');
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+
+    res.cookie('token', token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+    res.json({ message: 'User added successfully', data: savedUser });
   } catch (err) {
     res.status(400).send('Error:' + err.message);
   }
