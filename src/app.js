065f64deba37f6807e73293b8eee795f5ffea43b
Waleed <lido.murad@gmail.com>
2025-11-03 23:07:54 +0400

    Configured dotenv and added .env file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/database');
 const app = express();
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+require('dotenv').config();
 app.use(
   cors({
     origin: 'http://localhost:5173',
@@ -24,7 +25,7 @@ app.use('/', userRouter);
 connectDB()
   .then(() => {
     console.log('database connected successfully');
-    app.listen(7777, () => {
+    app.listen(process.env.PORT, () => {
       console.log('Server is successfully listening on port 7777...');
     });
   })
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -11,7 +11,7 @@ const userAuth = async (req, res, next) => {
 
     // Now verify the token
 
-    const decodedObj = await jwt.verify(token, 'DEV@Tinder$790');
+    const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
 
     const { _id } = decodedObj;
 
